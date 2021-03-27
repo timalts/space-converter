6a62e7eb2aeb93e36b5fa6cee1c00d5563c26ab7
Tim N <timothe.noel67@gmail.com>
2021-03-27 19:34:05 +0000

    Trying to make templates folder works

--- spaceconverter/settings.py
+++ spaceconverter/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
 TEMPLATE_DIRS = (
     os.path.join(SETTINGS_PATH, 'templates'),
 )
