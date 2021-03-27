d2167f1951468ebeab28a4599e17c0e6c7af9c34
Tim N <timothe.noel67@gmail.com>
2021-03-27 05:27:13 +0000

    Setup for first view

--- converter/views.py
+++ converter/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.shortcuts import render
+from django.http import HttpResponse
+def student_show(request):
+    x = []
+    for i in range(10):
+        x.append(i)
+    return HttpResponse("<h1>DataFlair Django Tutorials</h1>The Digits are {0}".format(x))
