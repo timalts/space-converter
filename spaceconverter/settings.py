7db5359d510ad6368831c2aedf25720b005524da
Tim N <timothe.noel67@gmail.com>
2021-03-27 19:32:33 +0000

    Trying to make templates folder works

--- spaceconverter/settings.py
+++ spaceconverter/settings.py
@@ -16,11 +16,9 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    os.path.join(BASE_DIR, 'templates').replace('\\','/'),
+    os.path.join(SETTINGS_PATH, 'templates'),
 )
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
