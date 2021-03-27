b276346fec47876c962aec0273b55f8a393e097b
Tim N <timothe.noel67@gmail.com>
2021-03-27 05:46:28 +0000

    Fix syntax error in settings.py

--- spaceconverter/settings.py
+++ spaceconverter/settings.py
@@ -19,7 +19,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_ROOT, 'templates').replace('\\','/'),
+    os.path.join(BASE_DIR, 'templates').replace('\\','/'),
 )
 
 # Quick-start development settings - unsuitable for production
