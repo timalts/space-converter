1233841b7e614b78a1105fdbe460d40d582d13e4
Tim N <timothe.noel67@gmail.com>
2021-03-27 21:09:23 +0000

    Add django form and try to retrieve it's content

---
+++ converter/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+class UserForm(forms.Form):
+    first_name= forms.CharField(max_length=100)
+    last_name= forms.CharField(max_length=100)
+    email= forms.EmailField()
--- converter/views.py
+++ converter/views.py
@@ -1,10 +1,22 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
+from .forms import UserForm
+
 
 def index(request):
-    var = "a"
-    template = loader.get_template('index/index.html')
-    context = {
-        'var': var,
-    }
-    return HttpResponse(template.render(context, request))
+    submitbutton= request.POST.get("submit")
+
+    firstname=''
+    lastname=''
+    emailvalue=''
+
+    form= UserForm(request.POST or None)
+    if form.is_valid():
+        firstname= form.cleaned_data.get("first_name")
+        lastname= form.cleaned_data.get("last_name")
+        emailvalue= form.cleaned_data.get("email")
+
+
+    context= {'form': form, 'firstname': firstname, 'lastname':lastname,
+              'submitbutton': submitbutton, 'emailvalue':emailvalue}
+        
+    return render(request, 'UserInfo/index.html', context)
