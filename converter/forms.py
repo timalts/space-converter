69ad4dfd87614d23854ce343cf58527bd75379e1
Tim N <timothe.noel67@gmail.com>
2021-03-30 02:34:22 +0100

    Fix syntax error in forms.py

--- converter/forms.py
+++ converter/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class ConvertForm(forms.Form):
-    content = forms.CharField(widget=forms.Textarea(attrs={"rows":5, "cols":20})
+    content = forms.CharField(widget=forms.Textarea(attrs={"rows":5, "cols":20}))
