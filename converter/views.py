e6706f4ccaa07c78892787d361adfe579b277fa3
Timothé Noël <timothe.noel67@gmail.com>
2021-03-30 02:41:05 +0100

    Add beginning of input parsing

--- converter/views.py
+++ converter/views.py
@@ -6,12 +6,13 @@ def index(request):
     submitbutton= request.POST.get("submit")
     content = ''
     form = ConvertForm(request.POST or None)
-
+    result = ''
     if form.is_valid():
         content = form.cleaned_data.get("content")
-
+        result = content.split(" ")
     context= {'form': form, 
             'content': content,
-            'submitbutton': submitbutton,}
+            'submitbutton': submitbutton,
+            'result': result}
         
     return render(request, 'index/index.html', context)
