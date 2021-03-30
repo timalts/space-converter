c5c019f973299d5ff9c0c5f67d084a626808fc58
Timothé Noël <timothe.noel67@gmail.com>
2021-03-30 03:04:59 +0100

    Change static file directory

--- spaceconverter/settings.py
+++ spaceconverter/settings.py
@@ -13,9 +13,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DIRNAME = os.path.abspath(os.path.dirname(__file__))
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 TEMPLATE_DIRS = (
     os.path.join(DIRNAME, 'templates/'),
 )
